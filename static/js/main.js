adb39c17b50d3d45a718ac1a9c1f7c5fb084254b
Gonzalo Galarza <ggalarza@evergreen.shipping.argentina>
2022-10-26 23:30:27 -0300

    Se repara desplegable y radio button

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,32 @@ if(btnDelete) {
         } )
     })
 }
+var select = document.querySelector(".seleccion");
+var selectOption = select.options[select.selectedIndex];
+var lastSelected = localStorage.getItem('select');
+
+if(lastSelected) {
+    select.value = lastSelected; 
+}
+
+select.onchange = function () {
+   lastSelected = select.options[select.selectedIndex].value;
+   console.log(lastSelected);
+   localStorage.setItem('select', lastSelected);
+}
+
+$(document).ready(function(){
+    var radios = document.getElementsByName("es_prestador");
+    var val = localStorage.getItem('es_prestador');
+    for(var i=0;i<radios.length;i++){
+      if(radios[i].value == val){
+        radios[i].checked = true;
+      }
+    }
+    $('input[name="es_prestador"]').on('change', function(){
+      localStorage.setItem('es_prestador', $(this).val());
+    
+    });
+  });
+
+
